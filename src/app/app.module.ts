81907fd944b0cb1ca071c6af8a1349aa755217dd
Ander Casado Zardoya <71841313+Casadich@users.noreply.github.com>
2024-05-24 13:18:53 +0200

    Created new flex containers for manage the position of the items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SousingListComponent } from './sousing-list/sousing-list.component';
 import { HousingListComponent } from './housing-list/housing-list.component';
+import { ContainerComponent } from './container/container.component';
+import { Item1Component } from './item1/item1.component';
+import { Item2Component } from './item2/item2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SousingListComponent,
-    HousingListComponent
+    HousingListComponent,
+    ContainerComponent,
+    Item1Component,
+    Item2Component
   ],
   imports: [
     BrowserModule
